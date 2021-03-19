4a22d21f7216cd8ea1e304f7dea1480a96ab2927
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-03-19 17:08:35 -0300

    delete categories functionality

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -7,10 +7,18 @@ router.get('/admin/categories/new', (req, res) => {
     res.status(200).render('admin/categories/new')
 });
 
+router.get('/admin/categories', (req, res) => {
+    Category.findAll().then((categories) => {
+        res.status(200).render('admin/categories/index', { categories: categories })
+    }).catch((err) => {
+        console.log(err)
+    });
+});
+
 router.post('/categories/save', (req, res) => {
     const title = req.body.title
 
-    if(title != undefined) {
+    if (title != undefined) {
         Category.create({
             title: title,
             slug: slugify(title)
@@ -22,13 +30,25 @@ router.post('/categories/save', (req, res) => {
     }
 });
 
-router.get('/admin/categories', (req, res) => {
-    Category.findAll().then((categories) => {
-        res.status(200).render('admin/categories/index', {categories: categories})
-    }).catch((err) => {
-        console.log(err)
-    });
+router.post('/categories/delete', (req, res) => {
+    const id = req.body.id
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/categories')
+            })
+        } else {
+            res.redirect('/admin/categories')
+        }
+    } else {
+        res.redirect('/admin/categories')
+    }
 });
+
 
 
 module.exports = router;
