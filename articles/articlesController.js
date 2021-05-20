6c34065c47dbcad2ab33f4933c0292e06d20b019
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-05-20 19:53:07 -0300

    listing and delete articles

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -5,14 +5,18 @@ const Article = require('./Article')
 const slugify = require('slugify')
 
 router.get("/admin/articles", (req, res) => {
-    res.render('admin/articles/index')
+    Article.findAll({
+        include: [{model: Category}]
+    }).then((articles) => {
+        res.render('admin/articles/index', {articles: articles})
+    })
 });
 
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories})
     })
-})
+});
 
 router.post('/articles/save', (req, res) => {
     const title = req.body.title
@@ -27,6 +31,25 @@ router.post('/articles/save', (req, res) => {
     }).then(() => {
         res.redirect('/admin/articles')
     })
-})
+});
+
+router.post('/articles/delete', (req, res) => {
+    const id = req.body.id
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/articles')
+            })
+        } else {
+            res.redirect('/admin/articles')
+        }
+    } else {
+        res.redirect('/admin/articles')
+    }
+});
 
 module.exports = router;
