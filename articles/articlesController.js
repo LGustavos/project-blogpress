f2a558fd940275b750087023ad1e32d29caa25d1
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-05-20 19:35:36 -0300

    Add articles listing page

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category')
+const Article = require('./Article')
+const slugify = require('slugify')
 
-router.get("/articles", (req, res) => {
-    res.status(200).send("Rota de artigos")
+router.get("/admin/articles", (req, res) => {
+    res.render('admin/articles/index')
 });
 
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories})
+    })
+})
+
+router.post('/articles/save', (req, res) => {
+    const title = req.body.title
+    const body = req.body.body
+    const category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles')
     })
 })
 
